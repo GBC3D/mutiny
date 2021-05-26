23e29952aa6a9c184b2d431574af2f024b72faae
Pramerios <pramereiosss@gmail.com>
2021-05-26 12:15:24 -0400

    Test
    i tested the test rectangular prism

---
+++ Mutiny/Assets/Scripts/test.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        bool test = false;
+
+        //Test
+        //This a test boi frfr
+        //Yeah
+        //testing...
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this boi update every frame wow
+
+        //testerjteidk
+
+        //testetsetst
+    }
+}
