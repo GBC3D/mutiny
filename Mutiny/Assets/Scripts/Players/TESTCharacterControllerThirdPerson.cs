f3dfb45fb50f8642e315e1091ceec12c04dca1a9
Jonathan Porter Squires <PyerterSquires@gmail.com>
2021-06-10 20:02:52 -0400

    Added character direction animation states

--- Mutiny/Assets/Scripts/Players/TESTCharacterControllerThirdPerson.cs
+++ Mutiny/Assets/Scripts/Players/TESTCharacterControllerThirdPerson.cs
@@ -5,38 +5,88 @@ using UnityEngine;
 public class TESTCharacterControllerThirdPerson : MonoBehaviour
 {
 
-    public CharacterController controller;
-    public Transform cam;
+    [SerializeField] public CharacterController controller; // Reference to CharacterController
+    [SerializeField] public Transform cam; // Camera transform
+    [SerializeField] public Transform displayRef; // Reference to the sprite display transform
+    [SerializeField] public Animator animator; // AnimatorController for the character display
 
-    public float speed = 6f;
-    public float turnSmoothTime = 0.1f;
-    float turnSmoothVelocity;
+    [SerializeField] public float speed = 6f; // Normal move speed
+    [SerializeField] public float sprintAdd = 1.0f; // Sprint speed addition
 
-    bool playerSprinting = false;
-    float sprintAdd = 1.0f;
+    // Bool for facing right with a get property
+    private bool facingRight = false;
+    public bool FacingRight { get { return facingRight; } }
+
+    private bool playerSprinting = false; // bool of player sprinting
+
+    private Vector3 requestMoveDirection; // direction to move in the next fixed update
 
     void Start()
     {
-        Cursor.visible = false;
+        // For the future, uncomment to remove cursor visibility
+        // Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Grab forward and size move values
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
+
+        // store in direction
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        if (direction.magnitude >= 0.1f)
-        {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y; 
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDirection.normalized * ( speed + sprintAdd ) * Time.deltaTime);
+        CheckMove(direction);
+        CheckFlip(horizontal);
+
+        if (animator != null)
+        {
+            if (vertical > 0)
+            {
+                if (horizontal != 0)
+                {
+                    animator.SetInteger("MoveDirection", 1);
+                }
+                else
+                {
+                    animator.SetInteger("MoveDirection", 2);
+                }
+            } else if (vertical < 0)
+            {
+                if (horizontal != 0)
+                {
+                    animator.SetInteger("MoveDirection", -1);
+                } else
+                {
+                    animator.SetInteger("MoveDirection", -2);
+                }
+            } else
+            {
+                animator.SetInteger("MoveDirection", 0);
+            }
+        }
+    }
+
+    private void CheckMove(Vector3 direction)
+    {
+        // Move if the magnitude of the direction is not 0
+        if (direction.magnitude != 0)
+        {
+            // Calculate target movement angle
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+
+            // Set the requested move direction
+            requestMoveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+        }
+        // Otherwise, don't move
+        else
+        {
+            requestMoveDirection = Vector3.zero;
         }
 
+        // Check if the player is sprinting and set corresponding speed
         if (Input.GetKey(KeyCode.LeftShift))
         {
             playerSprinting = true;
@@ -48,5 +98,44 @@ public class TESTCharacterControllerThirdPerson : MonoBehaviour
             sprintAdd = 0f;
         }
 
+        DoMove();
+    }
+
+    // Checks if the player display needs to flip directions
+    private void CheckFlip(float horizontal)
+    {
+        if (facingRight && horizontal < 0)
+        {
+            Flip();
+        } else if (!facingRight && horizontal > 0)
+        {
+            Flip();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // DoMove();
+    }
+
+    private void DoMove()
+    {
+        // Move the player
+        Vector3 movement = requestMoveDirection.normalized * (speed + sprintAdd) * Time.deltaTime;
+        controller.Move(movement);
+
+        // Update movement for animator
+        if (animator != null)
+            animator.SetFloat("MoveSpeed", movement.magnitude);
+    }
+
+    // Flip the player display
+    private void Flip()
+    {
+        facingRight = !facingRight;
+
+        Vector3 localScale = displayRef.transform.localScale;
+        localScale.x *= -1;
+        displayRef.transform.localScale = localScale;
     }
 }
