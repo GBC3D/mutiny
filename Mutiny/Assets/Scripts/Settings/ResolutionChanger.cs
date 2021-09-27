106cd9806bd9cc24ee5f00db992439a2cd6c9ba7
JBthe9th <jtb19833@uga.edu>
2021-09-27 10:25:29 -0400

    Menus & Title
    Added title screen and remade the main menu, working on a settings menu. Join and create game currently nonfunctional due to the buttons opening settings for some reason??

---
+++ Mutiny/Assets/Scripts/Settings/ResolutionChanger.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResolutionChanger : MonoBehaviour
+{
+
+    public void ChangeResolution()
+    {
+        int i = GetComponent<Dropdown>().value;
+        switch(i)
+        {
+            case 0:
+                break;
+            case 1:
+                Screen.SetResolution(1280, 720, false);
+                break;
+            case 2:
+                Screen.SetResolution(1366, 768, false);
+                break;
+            case 3:
+                Screen.SetResolution(1920, 1080, false);
+                break;
+            default:
+                Screen.SetResolution(1280, 720, false);
+                break;
+        }
+    }
+}
